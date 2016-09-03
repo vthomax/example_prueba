96973f7449cc775e175248fbdc257f5a02bf7158
Mac Owner <macowner@macs-macbook-pro.local>
2016-09-03 09:03:17 -0400

    Initial Commit

---
+++ prueba3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba3
+//
+//  Created by Mac Owner on 9/3/16.
+//  Copyright © 2016 iprueba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
