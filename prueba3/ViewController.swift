78564fe59641f871a4fa8a614af085beba9220e8
Mac Owner <macowner@macs-macbook-pro.local>
2016-09-03 09:49:04 -0400

    commentary

--- prueba3/ViewController.swift
+++ prueba3/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nameTextField(sender: AnyObject) {
+    messageLabel.text = "Hola \(nameTextField.text)"
+    view.endEditing(true)
+    }
+    
+    
+    
 
 }
 
